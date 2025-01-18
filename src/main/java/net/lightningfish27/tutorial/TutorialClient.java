4b50b1c978d90eee9d4e621e804f4b8540db79ce
LightningFish27 <lightningfish04@gmail.com>
2025-01-18 12:18:35 -0800

    Initial Commit

---
+++ src/main/java/net/lightningfish27/tutorial/TutorialClient.java
@@ -0,0 +1,10 @@
+package net.lightningfish27.tutorial;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class TutorialClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
